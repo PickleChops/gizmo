0fe0a94bcedb38fa3a8fb684f3442cdd4c45a3fe
bstratton <boydi@boydi.com>
2021-09-16 21:06:29 +0100

    Try import

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
+import generated from "@sveltejs/adapter-static";
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: require('@sveltejs/adapter-static')()
+		adapter: generated()
 	}
 };
 
